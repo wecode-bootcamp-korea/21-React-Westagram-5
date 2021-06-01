fcda1d7e777745398acdf1465a273fc215748b29
mac <mac@Bingbing.local>
2021-06-01 18:35:50 +0900

    ADD: Login버튼 색변경, 로그인데이터 저장 구현

--- src/Pages/ohyeonkwon/Login/Login.js
+++ src/Pages/ohyeonkwon/Login/Login.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.scss';
 //import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: '',
+      pw: '',
+      background: '',
+      disabled: true,
+    };
+  }
+
   goMain = () => {
     this.props.history.push('/MainH');
+  };
+
+  inputValue = e => {
+    // input에 작성한 값
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log(this.state.id, this.state.pw);
+  };
+
+  colorChange = () => {
+    if (this.state.id.includes('@') && this.state.pw.length > 4) {
+      this.setState({
+        background: 'black',
+        disabled: false,
+      });
+    }
   };
 
   render() {
@@ -18,20 +46,32 @@ class Login extends React.Component {
           <section className="login_section">
             <div className="id">
               <input
+                name="id"
                 className="id_input"
                 placeholder="전화번호,사용자 이름 또는 이메일"
                 type="text"
+                onKeyUp={this.colorChange}
+                onChange={this.inputValue}
               />
             </div>
             <div className="pw">
               <input
+                name="pw"
                 className="pw_input"
                 placeholder="비밀번호"
                 type="password"
+                onChange={this.inputValue}
+                onKeyUp={this.colorChange}
               />
             </div>
             <div className="login">
-              <button type="button" className="login_Btn" onClick={this.goMain}>
+              <button
+                type="button"
+                className="login_Btn"
+                style={{ backgroundColor: this.state.background }}
+                disabled={this.state.disabled}
+                onClick={this.goMain}
+              >
                 <span>로그인</span>
               </button>
             </div>
